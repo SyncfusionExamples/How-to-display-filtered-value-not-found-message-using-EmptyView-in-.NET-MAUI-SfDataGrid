824b60ea3fbbdecbf3c3e1cb63ed69b745bdfb93
Muthupandy-SF4685 <muthupandy.ramakrishnan@syncfusion.com>
2026-06-26 15:49:00 +0530

    Added Demo sample and update details in ReadMe file.

---
+++ SfDataGridSample/SfDataGridSample/Model/OrderInfo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SfDataGridSample
+{
+    public class OrderInfo
+    {
+        private string orderID;
+        private DateTime orderDate;
+        private string customer;
+        private string customerName;
+        private string shipCity;
+        private string shipCountry;
+
+        public string OrderID
+        {
+            get { return orderID; }
+            set { this.orderID = value; }
+        }
+
+        public string CustomerName
+        {
+            get { return this.customerName; }
+            set { this.customerName = value; }
+        }
+
+        public string ShipCountry
+        {
+            get { return shipCountry; }
+            set { this.shipCountry = value; }
+        }
+
+        public DateTime OrderDate
+        {
+            get { return this.orderDate; }
+            set { this.orderDate = value; }
+        }
+
+        public string ShipCity
+        {
+            get { return shipCity; }
+            set { this.shipCity = value; }
+        }
+
+        public OrderInfo(string orderId, string customerName, string country, DateTime orderDate, string shipCity)
+        {
+            this.OrderID = orderId;
+            this.customerName = customerName;
+            this.OrderDate = orderDate;
+            this.ShipCountry = country;
+            this.ShipCity = shipCity;
+        }
+    }
+}
